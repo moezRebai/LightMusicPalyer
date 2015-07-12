a611439caab5caa1c09a77acaf764f07de4cb1e1
moezRebai <moez.rebaii@gmail.com>
2015-07-12 14:09:25 +0100

    Add new project

--- LightMusicPlayer/LightMusicPlayer/MainWindow.xaml.cs
+++ LightMusicPlayer/LightMusicPlayer/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using LightMusicPlayer.Helpers;
 using LightMusicPlayer.ViewModel;
 
 namespace LightMusicPlayer
@@ -6,7 +12,7 @@ namespace LightMusicPlayer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
@@ -14,9 +20,199 @@ namespace LightMusicPlayer
         public MainWindow()
         {
             InitializeComponent();
-			//Hi
-			
+            NAudioEngine soundEngine = NAudioEngine.Instance;
+            soundEngine.PropertyChanged += NAudioEngine_PropertyChanged;
+
+            UIHelper.Bind(soundEngine, "CanStop", StopButton, Button.IsEnabledProperty);
+            UIHelper.Bind(soundEngine, "CanPlay", PlayButton, Button.IsEnabledProperty);
+            UIHelper.Bind(soundEngine, "CanPause", PauseButton, Button.IsEnabledProperty);
+
+            spectrumAnalyzer.RegisterSoundPlayer(soundEngine);
+            waveformTimeline.RegisterSoundPlayer(soundEngine);
+
+            LoadExpressionDarkTheme();
             Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+
+        #region NAudio Engine Events
+        private void NAudioEngine_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            NAudioEngine engine = NAudioEngine.Instance;
+            switch (e.PropertyName)
+            {
+                case "FileTag":
+                    if (engine.FileTag != null)
+                    {
+                        TagLib.Tag tag = engine.FileTag.Tag;
+                        if (tag.Pictures.Length > 0)
+                        {
+                            using (MemoryStream albumArtworkMemStream = new MemoryStream(tag.Pictures[0].Data.Data))
+                            {
+                                try
+                                {
+                                    BitmapImage albumImage = new BitmapImage();
+                                    albumImage.BeginInit();
+                                    albumImage.CacheOption = BitmapCacheOption.OnLoad;
+                                    albumImage.StreamSource = albumArtworkMemStream;
+                                    albumImage.EndInit();
+                                    albumArtPanel.AlbumArtImage = albumImage;
+                                }
+                                catch (NotSupportedException)
+                                {
+                                    albumArtPanel.AlbumArtImage = null;
+                                    // System.NotSupportedException:
+                                    // No imaging component suitable to complete this operation was found.
+                                }
+                                albumArtworkMemStream.Close();
+                            }
+                        }
+                        else
+                        {
+                            albumArtPanel.AlbumArtImage = null;
+                        }
+                    }
+                    else
+                    {
+                        albumArtPanel.AlbumArtImage = null;
+                    }
+                    break;
+                default:
+                    // Do Nothing
+                    break;
+            }
+
+        }
+        #endregion
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NAudioEngine.Instance.CanPlay)
+                NAudioEngine.Instance.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NAudioEngine.Instance.CanPause)
+                NAudioEngine.Instance.Pause();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NAudioEngine.Instance.CanStop)
+                NAudioEngine.Instance.Stop();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void LoadDefaultTheme()
+        {
+            DefaultThemeMenuItem.IsChecked = true;
+            DefaultThemeMenuItem.IsEnabled = false;
+            DarkBlueThemeMenuItem.IsChecked = false;
+            DarkBlueThemeMenuItem.IsEnabled = true;
+            ExpressionDarkMenuItem.IsChecked = false;
+            ExpressionDarkMenuItem.IsEnabled = true;
+            ExpressionLightMenuItem.IsChecked = false;
+            ExpressionLightMenuItem.IsEnabled = true;
+
+            Resources.MergedDictionaries.Clear();
+        }
+
+        private void LoadDarkBlueTheme()
+        {
+            DefaultThemeMenuItem.IsChecked = false;
+            DefaultThemeMenuItem.IsEnabled = true;
+            DarkBlueThemeMenuItem.IsChecked = true;
+            DarkBlueThemeMenuItem.IsEnabled = false;
+            ExpressionDarkMenuItem.IsChecked = false;
+            ExpressionDarkMenuItem.IsEnabled = true;
+            ExpressionLightMenuItem.IsChecked = false;
+            ExpressionLightMenuItem.IsEnabled = true;
+
+            Resources.MergedDictionaries.Clear();
+            ResourceDictionary themeResources = Application.LoadComponent(new Uri("DarkBlue.xaml", UriKind.Relative)) as ResourceDictionary;
+            Resources.MergedDictionaries.Add(themeResources);
+        }
+
+        private void LoadExpressionDarkTheme()
+        {
+            DefaultThemeMenuItem.IsChecked = false;
+            DefaultThemeMenuItem.IsEnabled = true;
+            DarkBlueThemeMenuItem.IsChecked = false;
+            DarkBlueThemeMenuItem.IsEnabled = true;
+            ExpressionDarkMenuItem.IsChecked = true;
+            ExpressionDarkMenuItem.IsEnabled = false;
+            ExpressionLightMenuItem.IsChecked = false;
+            ExpressionLightMenuItem.IsEnabled = true;
+
+            Resources.MergedDictionaries.Clear();
+            ResourceDictionary themeResources = Application.LoadComponent(new Uri("Skins/ExpressionDark.xaml", UriKind.Relative)) as ResourceDictionary;
+            Resources.MergedDictionaries.Add(themeResources);
+        }
+
+        private void LoadExpressionLightTheme()
+        {
+            DefaultThemeMenuItem.IsChecked = false;
+            DefaultThemeMenuItem.IsEnabled = true;
+            DarkBlueThemeMenuItem.IsChecked = false;
+            DarkBlueThemeMenuItem.IsEnabled = true;
+            ExpressionDarkMenuItem.IsChecked = false;
+            ExpressionDarkMenuItem.IsEnabled = true;
+            ExpressionLightMenuItem.IsChecked = true;
+            ExpressionLightMenuItem.IsEnabled = false;
+
+            Resources.MergedDictionaries.Clear();
+            ResourceDictionary themeResources = Application.LoadComponent(new Uri("ExpressionLight.xaml", UriKind.Relative)) as ResourceDictionary;
+            Resources.MergedDictionaries.Add(themeResources);
+        }
+
+        private void DefaultThemeMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            LoadDefaultTheme();
+        }
+
+        private void DarkBlueThemeMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            LoadDarkBlueTheme();
+        }
+
+        private void ExpressionDarkMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            LoadExpressionDarkTheme();
+        }
+
+        private void ExpressionLightMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            LoadExpressionLightTheme();
+        }
+
+        private void OpenFile()
+        {
+            Microsoft.Win32.OpenFileDialog openDialog = new Microsoft.Win32.OpenFileDialog();
+            openDialog.Filter = "(*.mp3)|*.mp3";
+            if (openDialog.ShowDialog() == true)
+            {
+                NAudioEngine.Instance.OpenFile(openDialog.FileName);
+                FileText.Text = openDialog.FileName;
+            }
+        }
+
+        private void OpenFileMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void CloseMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            NAudioEngine.Instance.Dispose();
         }
     }
 }
